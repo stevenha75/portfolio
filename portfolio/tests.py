397c4b0df84a3c14018dba9bd0757e5687fde869
Steven Ha <stevenha75@gmail.com>
2023-08-10 10:49:45 -0500

    Finish homepageview setup, some initial tests

--- portfolio/tests.py
+++ portfolio/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomepageTests(TestCase):
+    def setUp(self):
+        url = reverse("home")
+        self.response = self.client.get(url)
+
+    def test_url_exists_at_correct_location(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(self.response, "home.html")
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,16 @@
+from django.views import View
 from django.shortcuts import render
 
-# TODO: Write HomePageView
+
+class HomePageView(View):
+    def get(self, request):
+        # Retrieve data from models or perform custom queries
+        # data1 = Model1.objects.all()
+        # data2 = Model2.objects.filter(some_field="value")
+
+        context = {
+            # "data1": data1,
+            # "data2": data2,
+        }
+
+        return render(request, "home.html", context)
