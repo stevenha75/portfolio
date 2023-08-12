6272467d9f3b3d051b6305756dda4214046b484b
Steven Ha <stevenha75@gmail.com>
2023-08-11 21:27:58 -0500

    Media support

--- django_project/settings.py
+++ django_project/settings.py
@@ -126,6 +126,11 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
+# User uploaded media
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "static"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
